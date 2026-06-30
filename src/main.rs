e8c96779d94d4e51197e115d5bc023ee8045b6f6
caina <kina.lima1302@proton.me>
2026-06-30 13:26:14 -0300

    feat: added tokio and made it execute commands properly

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use color_eyre::eyre::Result;
 use kdl::KdlDocument;
-use niri_ipc::{Request, Response, socket::Socket};
+use niri_ipc::{Event, Request, Response, socket::Socket};
 
 #[derive(Parser)]
 struct Cli {
@@ -11,7 +11,8 @@ struct Cli {
     path: Option<String>,
 }
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     let config = Cli::parse();
     let path = config.path.map(|v| PathBuf::from(v)).unwrap_or(
         std::env::home_dir()
@@ -23,13 +24,50 @@ fn main() -> Result<()> {
     let Ok(content) = std::fs::read_to_string(path) else {
         return Ok(());
     };
-    let document: KdlDocument = content.parse().expect("Error on parsing kdl");
+
+    let document: KdlDocument = content.parse()?;
 
     let mut socket = Socket::connect()?;
     if let Ok(Response::Handled) = socket.send(Request::EventStream)? {
         let mut reader = socket.read_events();
         while let Ok(event) = reader() {
-            println!("{event:?}\nAki o KDL: {document:?}");
+            match event {
+                Event::WindowOpenedOrChanged { window }
+                    if let Some(ref id) = window.app_id
+                        && let Some(document) = document.get(id) =>
+                {
+                    let Some(children) = document.children() else {
+                        continue;
+                    };
+                    let Some(on_spawn_children) = children
+                        .get("on-spawn")
+                        .map(|node| node.children())
+                        .flatten()
+                    else {
+                        continue;
+                    };
+                    let commands: Vec<_> = on_spawn_children
+                        .nodes()
+                        .iter()
+                        .map(|child| {
+                            let mut out = tokio::process::Command::new("bash");
+                            out.arg("-c").arg(child.name().value());
+                            out
+                        })
+                        .collect();
+
+                    tokio::spawn(async {
+                        for mut cmd in commands {
+                            let mut child = cmd.spawn().expect("Error on initialize bash process");
+                            if let Err(e) = child.wait().await {
+                                println!("Error on waiting async command '{cmd:?}': {e}");
+                            }
+                        }
+                    });
+                    println!("{on_spawn_children:?}");
+                }
+                _ => {}
+            }
         }
     }
     Ok(())
