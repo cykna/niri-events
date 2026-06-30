fe922241b5788f2a19622d773fd96ba1305a125b
caina <kina.lima1302@proton.me>
2026-06-30 14:03:05 -0300

    refactor: refactored code to be on its own function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{collections::HashSet, path::PathBuf};
 use clap::Parser;
 use color_eyre::eyre::Result;
 use kdl::KdlDocument;
-use niri_ipc::{Event, Request, Response, socket::Socket};
+use niri_ipc::{Event, Request, Response, Window, socket::Socket};
 
 #[derive(Parser)]
 struct Cli {
@@ -12,66 +12,69 @@ struct Cli {
 }
 
 struct EventsState {
-    socket: Socket,
     document: KdlDocument,
     ids: HashSet<u64>,
 }
 
 impl EventsState {
-    pub fn new(doc: KdlDocument) -> Result<Self> {
-        Ok(Self {
+    pub fn new(doc: KdlDocument) -> Self {
+        Self {
             document: doc,
-            socket: Socket::connect()?,
+
             ids: HashSet::new(),
-        })
+        }
     }
+
+    fn did_window_spawn(&self, id: u64) -> bool {
+        !self.ids.contains(&id)
+    }
+
+    pub fn handle_events_of(&self, window: Window, handler: &str) {
+        let node = match () {
+            _ if let Some(id) = window.app_id => id,
+            _ if let Some(title) = window.title => title,
+            _ if let Some(pid) = window.pid => pid.to_string(),
+            _ => window.id.to_string(),
+        };
+        if let Some(doc) = self.document.get(&node)
+            && let Some(children) = doc.children()
+            && let Some(children) = children.get(handler).map(|node| node.children()).flatten()
+        {
+            let commands: Vec<_> = children
+                .nodes()
+                .iter()
+                .map(|child| {
+                    let mut out = tokio::process::Command::new("bash");
+                    out.arg("-c").arg(child.name().value());
+                    out
+                })
+                .collect();
+
+            tokio::spawn(async {
+                for mut cmd in commands {
+                    let mut child = cmd.spawn().expect("Error on initialize bash process");
+                    if let Err(e) = child.wait().await {
+                        println!("Error on waiting async command '{cmd:?}': {e}");
+                    }
+                }
+            });
+        };
+    }
+
     pub fn run(mut self) -> Result<()> {
-        let Ok(Response::Handled) = self.socket.send(Request::EventStream)? else {
+        let mut socket = Socket::connect()?;
+        let Ok(Response::Handled) = socket.send(Request::EventStream)? else {
             return Ok(());
         };
-        let mut reader = self.socket.read_events();
+        let mut reader = socket.read_events();
         while let Ok(event) = reader() {
             match event {
                 Event::WindowClosed { id } => {
                     self.ids.remove(&id);
                 }
-                Event::WindowOpenedOrChanged { window }
-                    if let Some(ref id) = window.app_id
-                        && let Some(document) = self.document.get(id) =>
-                {
-                    if self.ids.contains(&window.id) {
-                        continue;
-                    }
+                Event::WindowOpenedOrChanged { window } if self.did_window_spawn(window.id) => {
                     self.ids.insert(window.id);
-                    let Some(children) = document.children() else {
-                        continue;
-                    };
-                    let Some(on_spawn_children) = children
-                        .get("on-spawn")
-                        .map(|node| node.children())
-                        .flatten()
-                    else {
-                        continue;
-                    };
-                    let commands: Vec<_> = on_spawn_children
-                        .nodes()
-                        .iter()
-                        .map(|child| {
-                            let mut out = tokio::process::Command::new("bash");
-                            out.arg("-c").arg(child.name().value());
-                            out
-                        })
-                        .collect();
-
-                    tokio::spawn(async {
-                        for mut cmd in commands {
-                            let mut child = cmd.spawn().expect("Error on initialize bash process");
-                            if let Err(e) = child.wait().await {
-                                println!("Error on waiting async command '{cmd:?}': {e}");
-                            }
-                        }
-                    });
-                    println!("{on_spawn_children:?}");
+                    self.handle_events_of(window, "on-spawn");
                 }
                 _ => {}
             }
@@ -95,6 +98,6 @@ async fn main() -> Result<()> {
     };
 
     let document: KdlDocument = content.parse()?;
-    let state = EventsState::new(document)?;
+    let state = EventsState::new(document);
     state.run()
 }
